f310eb9529a410046b402544819160f409eab887
Othavio Rubim <orubim37@gmail.com>
2023-09-13 19:36:04 -0300

    first and second step of register feature

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,58 @@
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
+
+const ClaimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Deve conter ao menos 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, { message: 'Deve conter letras e hifens' })
+    .transform((value) => value.toLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
+
+export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(ClaimUsernameFormSchema),
+  })
+
+  const router = useRouter()
+
+  async function handlePreRegister(data: ClaimUsernameFormData) {
+    await router.push(`/register?username=${data.username}`)
+    console.log(data)
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handlePreRegister)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar usuario
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username?.message
+            : 'Digite o nome do usuario desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
+}
